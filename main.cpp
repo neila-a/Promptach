fbeebbb604196dd93598370721ecf945a0d762da
Neila <neilaspace@outlook.com>
2025-02-07 14:34:14 +0800

    🚸将读取用户名从调用whoami改为Qt

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <QDir>
 #include <QTime>
 #include "./line/consoleline.h"
 #include "./line/consoleside.h"
@@ -13,7 +14,8 @@ int main(
 
     QTime time = QTime::currentTime();
     QString timeFormat = settings.value("timeFormat", "HH:mm").toString();
-    ConsoleSide right = {{run("whoami", {}).trimmed(), USERNAME}, {time.toString(timeFormat), TIME}};
+    ConsoleSide right = {{QDir::home().dirName().trimmed(), USERNAME},
+                         {time.toString(timeFormat), TIME}};
 
     ConsoleLine line = {left, right};
     return line.write();
