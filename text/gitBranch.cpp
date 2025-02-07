df263b1df8e6cb9573c0565fcf75f40b00ab214a
Neila <neilaspace@outlook.com>
2025-02-07 16:06:53 +0800

    🚸将调用__git_ps1改为直接调用git

--- text/gitBranch.cpp
+++ text/gitBranch.cpp
@@ -4,7 +4,7 @@ coloredText gitBranch() {
     const bool isGit = isInGit();
     QString branch = "";
     if (isGit) {
-        branch = run("bash", {"-c", "source /usr/lib/git-core/git-sh-prompt && __git_ps1 %s"});
+        branch = run("git", {"branch", "--show-current"}).trimmed();
     }
     return {branch, GITBRANCH};
 }
