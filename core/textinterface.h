ad3c57fe437017014b3d3a42e13fd7417f336ae1
Neil Alen <neilaspace@outlook.com>
2026-01-17 10:24:33 +0800

    ✏️ 删除TextInterface::drawConfigInterface中的空行

--- core/textinterface.h
+++ core/textinterface.h
@@ -8,9 +8,7 @@ class TextInterface {
 public:
     virtual ~TextInterface() {}
     virtual coloredText getText() = 0; // Required
-    virtual void drawConfigInterface(QWidget* parent, std::function<void()> synced) {
-
-    } // Optional
+    virtual void drawConfigInterface(QWidget* parent, std::function<void()> synced) {} // Optional
 };
 
 QT_BEGIN_NAMESPACE
